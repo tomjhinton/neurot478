42fbf88a49a6af2f3e4ef2f206134a8fd79c82a0
Tom Hinton <tomjhinton@gmail.com>
2020-12-12 18:12:48 +0000

    webpack 5 might be working

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,35 @@ module.exports = {
     filename: 'bundle.js'
   },
   devtool: isProd ? false : 'source-map',
+  module: {
+    rules: [
+      { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.s(a|c)ss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      {test: /\.svg$/,
+        use: 'svg-inline-loader'},
+      {
+        test: /\.txt$/i,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [{loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+        }}]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase: path.resolve('src'),
     hot: true,
