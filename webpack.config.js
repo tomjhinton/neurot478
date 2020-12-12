278ef4bb6062b0b0bff2a3d1bb335ada4c517f58
Tom Hinton <tomjhinton@gmail.com>
2020-12-12 17:58:11 +0000

    fuck webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,17 @@
-const path = require('path')
+var path = require('path')
 const webpack = require('webpack')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-
 module.exports = {
+  mode: 'development',
   entry: './src/app.js',
   output: {
-    path: path.resolve('dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
 
-  },
-  devtool: 'source-maps',
-  module: {
-    rules: [
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-      {test: /\.svg$/,
-        loader: 'svg-inline-loader'},
-      {
-        test: /\.txt$/i,
-        use: 'raw-loader'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'fonts/'
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      }
-    ]
-  },
   devServer: {
     contentBase: path.resolve('src'),
     hot: true,
@@ -61,8 +32,10 @@ module.exports = {
 
     }),
     new webpack.ProvidePlugin({ 'window.decomp': 'poly-decomp' }),
-    new CopyWebpackPlugin([
-      { from: 'src/assets', to: 'assets' }
-    ])
+    new CopyWebpackPlugin( {
+      patterns: [
+        { from: 'src/assets', to: 'assets' }
+      ]}
+    )
   ]
 }
